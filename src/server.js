d077b23510489184cee824b51492268ca0684e9b
dim <dim@student.42seoul.kr>
2023-01-31 23:54:44 +0900

    Feat: 서버 미들웨어, 핸들러 설정 함수

--- src/server.js
+++ src/server.js
@@ -3,9 +3,29 @@ import express from "express";
 const PORT = 4000;
 const app = express();
 
-app.get('/', (req, res) => {
-	return res.end();
-} )
+
+const routerLogger = (req, res, next) => {
+	 console.log("PATH : ",  req.path);
+	 next();
+}
+
+const methodLogger = (req, res, next) => {
+	console.log("METHOD", req.method);
+	next();
+}
+
+const home = (req, res, next) => {
+	console.log("in home function");
+	return res.send("home");
+}
+
+const login = (req, res, next) => {
+	return res.send("login");
+}
+
+app.use(methodLogger, routerLogger);
+app.get('/', home);
+app.get('/login', login);
 
 app.listen(PORT, function() {
 	console.log("server listening on port 4000...");
