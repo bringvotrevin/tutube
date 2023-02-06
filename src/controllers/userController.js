c7d415543be48fdbed2132fdabee1529085942fd
dim <dim@student.42seoul.kr>
2023-02-06 23:40:54 +0900

    Feat: login 위한 더미user 데이터 생성

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -4,7 +4,7 @@ const user = {
 };
 
 export const login = (req, res) => res.render("home", user);
-export const join = (req, res) => res.render("join");
+export const join = (req, res) => res.render("join", user);
 export const editProfile = (req, res) => res.render("editProfile", {pageTitle: "editProfile"});
 // export const deleteProfile = (req, res) => res.render("deleteProfile");
 // export const detail = (req, res) => res.render("detail");
--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,4 +1,9 @@
-export const trending = (req, res) => res.render("home", {pageTitle: "trending videos"});
+const user = {
+	name: "dim",
+	login: false,
+};
+
+export const trending = (req, res) => res.render("home", {user: user});
 // export const watch = (req, res) => res.render("watch");
 export const edit = (req, res) => res.render("edit");
 // export const search = (req, res) => res.render("search");
