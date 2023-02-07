301efc8d5f9651fbba6464a97b89fcf2004241ae
dim <dim@student.42seoul.kr>
2023-02-07 21:41:01 +0900

    Feat: mixin에서 쓸 더미 video 데이터 생성

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -3,10 +3,29 @@ const user = {
 	login: false,
 };
 
-export const trending = (req, res) => res.render("home", {pageTitle: "home", user: user});
-// export const watch = (req, res) => res.render("watch");
-export const edit = (req, res) => res.render("edit", {pageTitle: "edit", user: user});
+let videos = [
+	{
+		title: "the first video",
+		id: 1,
+		rate: 1,
+		views: 100,
+		comments: 100,
+		createdAt: "1 min ago",
+	},
+	{
+		title: "the second video",
+		id: 2,
+		rate: 2,
+		views: 200,
+		comments: 200,
+		createdAt: "2 min ago",
+	}
+]
+
+export const trending = (req, res) => res.render("home", {pageTitle: "home", user, videos});
+export const watch = (req, res) => res.render("watch", {pageTitle: "watch", user});
+export const editVideo = (req, res) => res.render("editVideo", {pageTitle: "edit video", user});
+export const deleteVideo = (req, res) => res.render("deleteVideo", {pageTitle: "delete video", user});
+export const upload = (req, res) => res.render("upload", {pageTitle: "upload video", user});
 // export const search = (req, res) => res.render("search");
-// export const deleteVideo = (req, res) => res.render("deleteVideo");
 // export const share = (req, res) => res.render("share");
-// export const upload = (req, res) => res.render("upload");
