693dc6e06cbe2cf7fee3c426038eb96b737e2c32
dim <dim@student.42seoul.kr>
2023-02-07 16:19:55 +0900

    Refactor: user -> userRouter 변수명 수정

--- src/server.js
+++ src/server.js
@@ -1,8 +1,8 @@
 import express from "express";
 import morgan from "morgan";
 import globalRouter from "./router/globalRouter";
-import video from "./router/videoRouter";
-import user from "./router/userRouter";
+import videoRouter from "./router/videoRouter";
+import userRouter from "./router/userRouter";
 
 const PORT = 4000;
 const app = express();
@@ -13,8 +13,8 @@ app.set("views", process.cwd() + "/src/views" )
 
 app.use(logger);
 app.use('/', globalRouter);
-app.use('/video', video);
-app.use('/user', user);
+app.use('/video', videoRouter);
+app.use('/user', userRouter);
 
 app.listen(PORT, function() {
 	console.log("server listening on port 4000...");
