13a9b6b733097e1112dfec4c7ce6d5dad2b1f7bb
dim <dim@student.42seoul.kr>
2023-02-07 21:40:03 +0900

    Feat: video 넘버로 연결되는 url 파라미터로 받아서 처리

--- src/router/videoRouter.js
+++ src/router/videoRouter.js
@@ -1,13 +1,13 @@
 import express from "express";
-import {watch, edit, search, deleteVideo, share, upload} from "../controllers/videoController";
+import {watch, editVideo, search, deleteVideo, share, upload} from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
-// videoRouter.get('/watch', watch);
-videoRouter.get('/edit', edit);
+videoRouter.get('/:id(\\d+)', watch);
+videoRouter.get('/:id(\\d+)/edit', editVideo);
+videoRouter.get('/:id(\\d+)/delete', deleteVideo);
+videoRouter.get('/upload', upload);
 // videoRouter.get('/search', search);
-// videoRouter.get('/delete', deleteVideo);
 // videoRouter.get('/share', share);
-// videoRouter.get('/upload', upload);
 
 export default videoRouter;
