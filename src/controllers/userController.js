84f2841851576226bb960bb1c660a0321cc4d613
dim <dim@student.42seoul.kr>
2023-02-16 04:44:53 +0900

    Feat: 임시로 만들었던 더미데이터(user, videos) 삭제

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,12 +1,7 @@
-const user = {
-  name: 'dim',
-  login: false,
-};
-
-export const login = (req, res) => res.render('login', { pageTitle: 'login', user });
-export const logout = (req, res) => res.render('logout', { pageTitle: 'logout', user });
-export const join = (req, res) => res.render('join', { pageTitle: 'join', user });
-export const editProfile = (req, res) => res.render('editProfile', { pageTitle: 'editProfile', user });
-export const deleteProfile = (req, res) => res.render('deleteProfile', { pageTitle: 'deleteProfile', user });
+export const login = (req, res) => res.render('login', { pageTitle: 'login' });
+export const logout = (req, res) => res.render('logout', { pageTitle: 'logout' });
+export const join = (req, res) => res.render('join', { pageTitle: 'join' });
+export const editProfile = (req, res) => res.render('editProfile', { pageTitle: 'editProfile' });
+export const deleteProfile = (req, res) => res.render('deleteProfile', { pageTitle: 'deleteProfile' });
 // export const detail = (req, res) => res.render("detail");
 // export const changePassword = (req, res) => res.render("changePassword");
