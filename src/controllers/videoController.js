fa42bf7c8d54136bd68f442b87ed93f2d9550936
dim <dim@student.42seoul.kr>
2023-02-17 23:44:50 +0900

    Feat: db에서 비디오 읽어오기 async,await처리

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,4 +1,9 @@
-export const home = (req, res) => res.render('home', { pageTitle: 'home' });
+import Video from '../models/Video';
+
+export const home = async (req, res) => {
+  const videos = await Video.find({});
+  res.render('home', { pageTitle: 'home', videos });
+};
 export const watch = (req, res) => {
   res.render('watch', { pageTitle: 'watch' });
 };
